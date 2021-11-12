df1f486f8f737962d16f598023cf7f836ce1a928
PhivPap <phoivos@windowslive.com>
2021-11-12 16:57:36 +0100

    Removed doc from headers

--- List/List.h
+++ List/List.h
@@ -26,36 +26,3 @@ void* ListIterator_peak(ListIterator* this);
 void ListIterator_reset(ListIterator* this);
 
 #endif
-
-/* DOC
-    List:
-        Implementation of a sigle linked list with O(1) push_front and append functions.
-        Unchecked malloc(...).
-
-        List_clear(...) 
-            - clears the list (does not call free() on data).
-
-        List_clear_free(...)
-            - danger!
-            - clears the list and frees all elements. Erroneous if elements we not allocated from heap.
-
-        List_destroy(...*)
-            - calls List_clear(...)
-
-        List_destroy_free(...)
-            - calls List_clear_free(...)
-
-
-    ListIterator:
-        Implementation of a read-only List Iterator..
-        Unchecked malloc().
-
-        ListIterator_peak(...) 
-            - returns NULL if the iterator has reached the end of the list.
-            - Erroneous to call after its List has been modified, call ListIterator_reset(...).
-
-        ListIterator_next(...)
-            - returns NULL if the iterator has reached the end of the list.
-            - returns current element of the list. After the call, the iterator is pointing to the next element.
-            - Erroneous to call after its List has been modified, call ListIterator_reset(...).
- */
