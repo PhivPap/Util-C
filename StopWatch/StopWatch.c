6200620313e39397476185d8f1211ece3177003c
PhivPap <phoivos@windowslive.com>
2022-08-25 17:49:38 +0200

    Change StopWatch to CLOCK_MONOTONIC

--- StopWatch/StopWatch.c
+++ StopWatch/StopWatch.c
@@ -1,9 +1,9 @@
 #include "StopWatch.h"
-#include <sys/time.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct StopWatch {
-    struct timeval check_point;
+    struct timespec check_point;
     double prev_elapsed_sec;
     unsigned char is_running;
 };
@@ -12,7 +12,7 @@ StopWatch* StopWatch_new(){
     StopWatch* this = malloc(sizeof(StopWatch));
     if(!this)
         return NULL;
-    gettimeofday(&this->check_point, 0);
+    clock_gettime(CLOCK_MONOTONIC, &this->check_point);
     this->prev_elapsed_sec = 0.0;
     this->is_running = 1;
     return this;
@@ -25,13 +25,13 @@ void StopWatch_destroy(StopWatch* this){
 double StopWatch_elapsed_sec(StopWatch* this){
     if(!this->is_running)
         return this->prev_elapsed_sec;
-    struct timeval timer_now;
-    gettimeofday(&timer_now, 0);
-    return this->prev_elapsed_sec + (double)(timer_now.tv_sec - this->check_point.tv_sec) + ((double)(timer_now.tv_usec - this->check_point.tv_usec)/ 1000000.0);
+    struct timespec timer_now;
+    clock_gettime(CLOCK_MONOTONIC, &timer_now);
+    return this->prev_elapsed_sec + (double)(timer_now.tv_sec - this->check_point.tv_sec) + ((double)(timer_now.tv_nsec - this->check_point.tv_nsec)/ 1000000000.0);
 }
 
 void StopWatch_reset(StopWatch* this){
-    gettimeofday(&this->check_point, 0);
+    clock_gettime(CLOCK_MONOTONIC, &this->check_point);
     this->prev_elapsed_sec = 0.0;
     this->is_running = 1;
 }
@@ -39,16 +39,16 @@ void StopWatch_reset(StopWatch* this){
 void StopWatch_pause(StopWatch* this){
     if(!this->is_running)
         return;
-    struct timeval timer_now;
-    gettimeofday(&timer_now, 0);
-    this->prev_elapsed_sec += (double)(timer_now.tv_sec - this->check_point.tv_sec) + ((double)(timer_now.tv_usec - this->check_point.tv_usec)/ 1000000.0);
+    struct timespec timer_now;
+    clock_gettime(CLOCK_MONOTONIC, &timer_now);
+    this->prev_elapsed_sec += (double)(timer_now.tv_sec - this->check_point.tv_sec) + ((double)(timer_now.tv_nsec - this->check_point.tv_nsec)/ 1000000000.0);
     this->is_running = 0;
 }
 
 void StopWatch_resume(StopWatch* this){
     if(this->is_running)
         return;
-    gettimeofday(&this->check_point, 0);
+    clock_gettime(CLOCK_MONOTONIC, &this->check_point);
     this->is_running = 1;
 }
 
