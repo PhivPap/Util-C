ba94d4d2ba60590ca60257236b73847f9f6f615e
PhivPap <phoivos@windowslive.com>
2022-12-07 23:04:11 +0100

    deserializers now correctly allocate heap

--- Serialization/deserializers.c
+++ Serialization/deserializers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void string_deserializer(FILE* fp, void** str_ref) {
     char* str = NULL;
@@ -8,13 +9,16 @@ void string_deserializer(FILE* fp, void** str_ref) {
 }
 
 void byte_deserializer(FILE* fp, void** byte_ref) {
+    *byte_ref = malloc(1);
     fread(*byte_ref, 1, 1, fp);
 }
 
 void byte4_deserializer(FILE* fp, void** word_ref) {
+    *word_ref = malloc(4);
     fread(*word_ref, 4, 1, fp);
 }
 
 void byte8_deserializer(FILE* fp, void** dword_ref) {
+    *dword_ref = malloc(8);
     fread(*dword_ref, 8, 1, fp);
 }
